9e672b87b8db3559a9fb977306d34e3e18e1a052
dxt99 <ericlouis19x@gmail.com>
2022-12-02 14:28:20 +0700

    feat: implemented occ template class

---
+++ src/db/occ-dbms.py
@@ -0,0 +1,26 @@
+from dbms import DBMS
+
+class OCC(DBMS):
+    def __init__():
+        self.states = {} # each transaction's state
+        self.completed = [] # transactions that have been completed
+        self.started = [] # transaction that have been started
+
+    def start(transaction:str) -> bool:
+        if (transaction in self.started):
+            return False
+        
+        self.started.append(transaction)
+        return True
+    
+    def commit(transaction:str) -> bool:
+        if (transaction in self.completed) or not (transaction in self.started):
+            return False
+        # TODO: start validation
+
+    
+    def read(transaction:str, key:str) -> (bool, str):
+        raise NotImplementedError()
+    
+    def write(transaction:str, key:str, value:str) -> (bool, str):
+        raise NotImplementedError()
